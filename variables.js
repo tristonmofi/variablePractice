eaa558a571cd98175f8301468d87c0c219353512
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-25 21:24:19 -0600

    Variable practice and console.log statements

--- variables.js
+++ variables.js
@@ -1,8 +1,16 @@
 var name = "Triston";
 var lastName = "Filippi";
 
-var currentAge = 28
-var yearBorn = 1995
+var currentAge = 28;
+var yearBorn = 1995;
 
-var workSucked = true
-var codingDoesNot = true
+var workSucked = true;
+var codingDoesNot = true;
+
+name = "SweetT";
+lastName = "Modlin";
+
+console.log(name)
+console.log(workSucked)
+console.log(codingDoesNot)
+console.log(lastName)
